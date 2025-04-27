7fd1a52d1ffd3677c0de5653f14629f45729c275
glitch-glitz <fullstack.hub.devx@gmail.com>
2025-04-27 15:13:30 +0300

    Done

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Ever since I was a jit I knew I was the sht</p>
+      <img src={image} alt="I made this"></img>
+    </div>
+  );
 }
 
 export default About;
